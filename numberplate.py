e65b5970ce766530300c48ca1ac64dc5e4a0444e
Andrew Mulholland <andrew@bash.sh>
2014-07-30 12:18:26 -0400

    mornings work... got mask working correctly and added in the initial flood fill

--- numberplate.py
+++ numberplate.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
 import cv2
-import numpy as np
 from matplotlib import pyplot as plt
+import numpy as np
+import random
 
 filepath = "/Users/andy/work/headon/_M2B3091.jpg"
 
@@ -23,7 +24,7 @@ morphed_img = cv2.morphologyEx(threshold_img,cv2.MORPH_CLOSE,element)
 morphed_img2 = morphed_img.copy()
 #morphed_img2 = cv2.dilate(morphed_img,kernel)
 #morphed_img2 = cv2.morphologyEx(threshold_img,cv2.MORPH_CLOSE,kernel)
-contours, hierarchy = cv2.findContours(morphed_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+morphed_img, contours, hierarchy = cv2.findContours(morphed_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 #plt.imshow(cont_img, cmap = 'gray')
 
 def verifySizes(mr):
@@ -66,7 +67,7 @@ print len(newcontours)
 print morphed_img.shape
 
 for rect in rects:
-  box = cv2.cv.BoxPoints(rect)
+  box = cv2.boxPoints(rect)
   x = rect[0][0]
   y = rect[0][1]
   width = float(rect[1][0])
@@ -77,8 +78,27 @@ for rect in rects:
   result = cv2.circle(morphed_img,(centerx,centery), 3, (0,255,0), -1)
   minSize = min(width/2, height/2)
 
-  mask = np.zeros((morphed_img.shape[0]+2, morphed_img.shape[1] +2))
+  mask = np.zeros((morphed_img.shape[0]+2, morphed_img.shape[1] +2),np.uint8)
+  mask[:] = 0
   print mask.shape
+  print type(mask)
+  #mask = cv2.drawContours(mask,newcontours[0],0,255,-1)
+  print centerx
+  lo,hi = 30,30
+  connectivity = 4
+  flags = connectivity
+  flags |= cv2.FLOODFILL_FIXED_RANGE
+  seeds = np.zeros((5,2),np.int16)
+  for i in (1,2):
+    print random.randrange(int(centerx-minSize/2),int(centerx+minSize/2))
+    seeds[i][0] = random.randrange(int(centerx-minSize/2),int(centerx+minSize/2))
+    seeds[i][1] = random.randrange(int(centery-minSize/2),int(centery+minSize/2))
+    #seeds[i]=[int(val) for val in seeds[i]]
+    print seeds[i]
+    cv2.floodFill(img,mask,tuple(seeds[i]),(255,255,255),lo,hi,flags)
+    cv2.circle(img,tuple(seeds[i]),2, (0, 0, 255), -1)
+
+  cv2.imshow('circle', img)
 
 
 #cv2.drawContours(morphed_img,newcontours,-1,(128,255,255),3)
@@ -90,5 +110,5 @@ for rect in rects:
 #plt.imshow(morphed_img)
 #plt.show()
 #cv2.imshow('image',sobel_img)
-#ccv2.waitKey(0)
-#cv2.destroyAllWindows()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
