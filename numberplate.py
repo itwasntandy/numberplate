3fe06b7769778e91793a37b72a3ead1fb6d22a3f
Andrew Mulholland <andrew@bash.sh>
2014-08-06 12:05:33 -0400

    woohooo!!!!

--- numberplate.py
+++ numberplate.py
@@ -6,19 +6,20 @@ import numpy as np
 import random
 import copy
 
-filepath = "./_M2B3091.jpg" #works well on this one
-filepath = "./_M2B3097.jpg"
+#filepath = "./_M2B3091.jpg" #works well on this one
+#filepath = "./_M2B3097.jpg"
 filepath = "./_DNF0596.jpg"
 #filepath = "./_DNF0618_cropped.jpg"
 #filepath = "./_DNF0618.jpg" #a hard one
 #filepath = "./_DNF0630.jpg"
 #filepath = "_N7B2040.jpg"
 #filepath = "_DNF0648.jpg"
-filepath = "_DNF0395.jpg"
-filepath= "_DNF0612.jpg"
+#filepath = "_DNF0395.jpg"
+#filepath= "_DNF0612.jpg"
 
 
 img = cv2.imread(filepath,0)
+ffimg = copy.deepcopy(img)
 #blur_img = cv2.blur(img,(10,10))
 blur_img = cv2.blur(img,(5,5))
 
@@ -36,8 +37,8 @@ morphed_img = cv2.erode(morphed_img,kernel,iterations = 4)
 morphed_img = cv2.dilate(morphed_img,kernel,iterations = 9)
 
 
-plt.imshow(morphed_img,'gray')
-plt.show()
+#plt.imshow(morphed_img,'gray')
+#plt.show()
 #kernel = np.ones((8,8),np.uint8)
 #morphed_img2 = morphed_img.copy()
 #morphed_img2 = cv2.dilate(morphed_img,kernel)
@@ -46,7 +47,7 @@ morphed_img, contours, hierarchy = cv2.findContours(morphed_img,cv2.RETR_TREE,cv
 #morphed_img, contours, hierarchy = cv2.findContours(morphed_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 #plt.imshow(morphed_img,'gray')
 #plt.show()
-  
+
 
 def verifySizes(mr):
   error = 0.4
@@ -77,9 +78,9 @@ rects = []
 newcontours = []
 
 for contour in contours:
-  
+
   mr = cv2.minAreaRect(contour)
-  if (verifySizes(mr)):  
+  if (verifySizes(mr)):
   #if (verifySizes(mr)) and cv2.contourArea(contour)/len(contour)<100:
     #print "contour area", cv2.contourArea(contour)
     rects.append(mr)
@@ -108,11 +109,11 @@ for numContour,rect in enumerate(rects):
   mask[:] = 0
   #cv2.fillConvexPoly(mask,box.astype(np.int32),(255))
 
-  masksm= np.zeros((morphed_img.shape[0], morphed_img.shape[1] ),np.uint8)
-  masksm[:] = 0
-  cv2.fillConvexPoly(masksm,box,(255))
+  #masksm= np.zeros((morphed_img.shape[0], morphed_img.shape[1] ),np.uint8)
+  #masksm[:] = 0
+  #cv2.fillConvexPoly(masksm,box,(255))
 
-  maskedimg = img & masksm
+  #maskedimg = img & masksm
 
   #cv2.drawContours(mask,newcontours[numContour],-1,255,-1)
   # print mask.shape
@@ -124,10 +125,10 @@ for numContour,rect in enumerate(rects):
   flags = connectivity
   flags |= (255 << 8)
   flags |= cv2.FLOODFILL_FIXED_RANGE
-  #flags |= cv2.FLOODFILL_MASK_ONLY
+  flags |= cv2.FLOODFILL_MASK_ONLY
 
   #flags |= cv2.FLOODFILL_MASK_ONLY
-  numseeds  =5
+  numseeds  = 5
   seeds = np.zeros((numseeds,2),np.int16)
   for i in range(numseeds):
     # print random.randrange(int(centerx-minSize/2),int(centerx+minSize/2))
@@ -137,55 +138,76 @@ for numContour,rect in enumerate(rects):
     # print seeds[i]
 
     #cv2.floodFill(img,mask,tuple(seeds[i]),(255,255,255),lo,hi,flags)
-    cv2.floodFill(maskedimg,mask,tuple(seeds[i]),(255,255,255),lo,hi,flags)
+    cv2.floodFill(img,mask,tuple(seeds[i]),(255,255,255),lo,hi,flags)
+
+  floodPoints = np.where(mask == 255)
+  floodPoints = [floodPoints[1],floodPoints[0]]
+  floodPoints = np.transpose(floodPoints)
+
+  newMR = cv2.minAreaRect(floodPoints)
+
+  newbox = np.int0(cv2.boxPoints(newMR))
+  #plt.imshow(mask,'gray')
+  #plt.show()
+  if (not verifySizes(newMR)):
+     continue
+  else:
+    pass
+
+
+
 
   # find the enclosing non rotated rectangle
   def enclosingRect(box):
     xMin = min([box[i][0] for i in range(3)])
-    xMin = max(xMin,0)
     xMax = max([box[i][0] for i in range(3)])
-    xMax = min(xMax,img.shape[1])
     yMin = min([box[i][1] for i in range(3)])
-    yMin = max(yMin,0)
     yMax = max([box[i][1] for i in range(3)])
-    yMax = min(yMax,img.shape[0])
     return (xMin,xMax,yMin,yMax)
 
 
-  xMin,xMax,yMin,yMax = enclosingRect(box)
+  #
+  #
+  xMin,xMax,yMin,yMax = enclosingRect(newbox)
+
+  center = ((xMin+xMax)/2,(yMin+yMax/2))
+
+  rotationMatrix = cv2.getRotationMatrix2D(center,newMR[-1],1)
 
 
 
-  # calculate a new rectangle for just the flood filled points
-  pointsofinterest = []
-  for x in range(xMin, xMax):
-    for y in range(yMin, yMax):
-      # we flip x y here because .shape returns y first.. confusing eh?
-      try:
-        if maskedimg[y][x] == 255:
-          pointsofinterest.append([x,y])
-      except:
-        print "box is :", box
-        print "shape is:", maskedimg.shape
-        print "x,y:", x, y
-        break
-
-  #print "there are: ", len(pointsofinterest), " ponits of interest"
-  newMR = cv2.minAreaRect(np.array(pointsofinterest))
-
-  x = newMR[0][0]
-  y = newMR[0][1]
-  width = float(newMR[1][0])
-  height = float(newMR[1][1])
-  centerx = int(x)
-  centery = int(y)
-
-  newbox = np.int0(cv2.boxPoints(newMR))
-  #print newbox
-
+  #
+  #  # calculate a new rectangle for just the flood filled points
+  #  pointsofinterest = []
+  #  for x in range(xMin, xMax):
+  #    for y in range(yMin, yMax):
+  #      # we flip x y here because .shape returns y first.. confusing eh?
+  #      try:
+  #        if maskedimg[y][x] == 255:
+  #          pointsofinterest.append([x,y])
+  #      except:
+  #        print "box is :", box
+  #        print "shape is:", maskedimg.shape
+  #        print "x,y:", x, y
+  #        break
+  #
+  #  #print "there are: ", len(pointsofinterest), " ponits of interest"
+  #  newMR = cv2.minAreaRect(np.array(pointsofinterest))
+  #
+  #  x = newMR[0][0]
+  #  y = newMR[0][1]
+  #  width = float(newMR[1][0])
+  #  height = float(newMR[1][1])
+  #  centerx = int(x)
+  #  centery = int(y)
+  #
+  #  newbox = np.int0(cv2.boxPoints(newMR))
+  #  #print newbox
+  #
 
   #newimg = cv2.rectangle(img,(int(centerx - width/2),int(centery - height/2)),(int(centerx + width/2),int(centery+ height/2)),(128,128,128))
   newimg = cv2.drawContours(img,[newbox],0,(128,128,128), 5)
+
 
   #new_img = cv2.circle(img,tuple(seeds[0]),100, (255, 255, 255), 10)
   #new_img = cv2.circle(img,(centerx,centery),10, (0, 0,0 ), 10)
@@ -200,7 +222,7 @@ for numContour,rect in enumerate(rects):
   # print seeds[0]
 
   #cv2.imshow('circle', img)
-cv2.drawContours(img,contours,-1,(0,255,0),3)
+#cv2.drawContours(img,contours,-1,(0,255,0),3)
 if(len(newimg)):
   cv2.imshow('circle', newimg)
 else:
