530f16cc79ee2355a5f428cf73868a80e7c1260b
Andrew Mulholland <andrew@bash.sh>
2014-08-25 10:53:50 -0400

    reviewed the demo script

--- demo.sh
+++ demo.sh
@@ -2,49 +2,48 @@
 
 echo "starting Demo"
 
-echo "open _M2B3099.jpg"
-open ./images/_M2B3099.jpg
-
-echo "running read-single finder"
-echo "..."
-python read-single.py images/_M2B3099.jpg
 echo ""
-read -p "press enter to continue"
+open ./images/_M2B3099.jpg
+echo "running python read-single.py images/_M2B3099.jpg..."
+echo ""
+python read-single.py images/_M2B3099.jpg
+
+echo ""
+read -p "next..."
 echo ""
 open images/_M2B3097.jpg
-
 echo ""
-
-echo "running read-single finder"
-echo "..."
+echo "running python read-single.py images/_M2B3097.jpg..."
+echo ""
 python read-single.py images/_M2B3097.jpg 
-
-read -p "press enter"
+echo ""
+read -p "next..."
 open images/_M2B3115.jpg
 echo ""
-echo "running read-single finder"
-echo "..."
+echo "running python read-single.py images/_M2B3115.jpg..."
+echo ""
 python read-single.py images/_M2B3115.jpg
-
-read -p "press enter"
+echo ""
+read -p "next..."
 open images/_M2B3096.jpg
 echo ""
-echo "running read-single finder"
-echo "..."
+echo "running python read-single.py images/_M2B3096.jpg..."
+echo ""
 python read-single.py images/_M2B3096.jpg
-
-#read -p "press enter"
+echo ""
+#read -p "next..."
 #open images/_M2D8255.jpg
 #echo ""
 #echo "running read-single finder"
 ##echo "..."
 #python read-single.py images/_M2D8255.jpg
-
-read -p "press enter"
+echo ""
+read -p "next..."
 open images/_M2D8388.jpg
 echo ""
-echo "running read-single finder"
-echo "..."
+echo "running python read-single.py images/_M2D8388.jpg..."
+echo ""
 python read-single.py images/_M2D8388.jpg
+echo ""
 
 
